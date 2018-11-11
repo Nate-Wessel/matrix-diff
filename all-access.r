12dbc24f6bde95612592aeb721e21ee3cb6a33cb
Nate-Wessel <bike756@gmail.com>
2018-11-11 10:18:01 -0500

    garbage collection

--- all-access.r
+++ all-access.r
@@ -5,8 +5,6 @@ dimnames(A)[[1]] = c('sched','retro')
 dimnames(A)[[2]] = c('cum','negexp') # function names
 dimnames(A)[[3]] = c('A','A_o','A_ot') # aggregations
 
-# set the weights
-w1 = w2 = 'zones'
 # for each of these access functions
 for( func_name in dimnames(A)[[2]] ){
 	# get the function from the name
@@ -22,6 +20,7 @@ for( func_name in dimnames(A)[[2]] ){
 	A[['sched',func_name,'A_o']] = s_acc[[2]]
 	A[['retro',func_name,'A_ot']] = r_acc[[3]]
 	A[['sched',func_name,'A_ot']] = s_acc[[3]]
+	# this is but shouldn't be necessary
+	gc()
 }
-remove(func,func_name,w1,w2,s_acc,r_acc)
-
+remove(func,func_name,s_acc,r_acc)
