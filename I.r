88e641bab01993fae4523e43955b27b836c3c0b3
Nate-Wessel <bike756@gmail.com>
2018-11-20 19:49:06 -0500

    streamlining

--- I.r
+++ I.r
@@ -1,30 +1,9 @@
-# get access data
-agency = 'Muni'
-load(paste0('~/dissdata/R/',agency,'_A.RData'))
-l2 = function(x){log(x,2)} # log base 2
+# generate figure I showing distributions of $I$ for each agency 
+# with two access functions
 
-# database connection
-library('RPostgreSQL')
-library('postGIStools')
-library('spdep')
-# grab OD points from the DB
-od_table = switch(agency,'TTC'='ttc_od','JTA'='jv_od','Muni'='muni_od','MBTA'='mbta_od')
-con <- dbConnect( PostgreSQL(), dbname='diss', user='nate', host='localhost', password='mink')
-od <- get_postgis_query(con,paste('SELECT * FROM',od_table,'ORDER BY uid'),geom_name='loc_geom')
-dbDisconnect(con)
-
-# Sphere of Influence Graph as defined here: 
-# https://cran.r-project.org/web/packages/spdep/vignettes/nb.pdf
-nb = tri2nb(od)
-#plot.nb(nb,coordinates(od))
-nb = graph2nb(soi.graph(nb, coordinates(od)))
-#plot.nb(nb,coordinates(od))
-
-# measure error hourly
-eAo = A[['retro','cum','30','A_oh']] / A[['sched','cum','30','A_oh']]
-#eAoh = exp(apply(log(A[['retro','cum','30','A_oh']] / A[['sched','cum','30','A_oh']]),2,mean))
-eAh = A[['retro','cum','30','A_h']] / A[['sched','cum','30','A_h']]
-
+# log base 2
+l2 = function(x){log(x,2)} 
+# calculate I on a given vector where graphs and all that are already defined
 mymoran = function(x){
 	mi = moran(
 		x=x,
@@ -35,6 +14,41 @@ mymoran = function(x){
 	return(mi$I)
 }
 
-ik = apply(l2(eAo),2,mymoran)
-plot(l2(eAh),ik)
-abline(lm(ik~l2(eAh)))
+# database connection
+library('RPostgreSQL')
+library('postGIStools')
+con <- dbConnect( PostgreSQL(), dbname='diss', user='nate', host='localhost', password='mink')
+# moran
+library('spdep')
+
+# main loop
+x = list()
+j = 1
+for( agency in c('TTC','JTA','MBTA','Muni')){
+	# get the data
+	load(paste0('~/dissdata/R/',agency,'_A.RData'))
+	# grab OD points from the DB
+	od_table = switch(agency,'TTC'='ttc_od','JTA'='jv_od','Muni'='muni_od','MBTA'='mbta_od')
+	od <- get_postgis_query(con,paste('SELECT * FROM',od_table,'ORDER BY uid'),geom_name='loc_geom')
+	# Sphere of Influence Graph as defined here: 
+	nb = graph2nb(soi.graph(tri2nb(od), coordinates(od)))
+	plot.nb(nb,coordinates(od))
+	for(func in c('negexp','cum')){
+		param = switch(func,'cum'='45',negexp='30')
+		eAo = A[['retro',func,param,'A_oh']] / A[['sched',func,param,'A_oh']]
+		x[[j]] = apply(l2(eAo),2,mymoran)
+		#x[j] = quantile(i_h,c(0,.25,.5,.75,1))
+		j = j + 1
+	}
+}
+dbDisconnect(con)
+
+library('beeanplot')
+beanplot(x,horizontal=T,method='stack',side='second',cut=1,bw=0.02,col=c(adjustcolor('red',alpha=.2)))
+
+
+# set up the plot
+y = 1:8
+plot( x=c(x[,1],x[,3],x[,5]), y=c(y,y,y) )
+segments( x0=x[,2],x1=x[,4],y0=y,y1=y )
+abline(lm(i~l2(eAh)))
