6b09a2c7c3d9c030f76f84be6ee4a540a6d552c5
Nate-Wessel <bike756@gmail.com>
2018-11-22 19:05:56 -0500

    some minor changes

--- param-search.r
+++ param-search.r
@@ -1,20 +1,19 @@
 # load the data
-agency = 'TTC'
-load(paste0('~/',agency,'.RData'))
+agency = 'Muni'
+load(paste0('~/dissdata/R/',agency,'_A.RData'))
+gc()
 
-# log base 2
-l2 = function(x){log(x,2)}
+l2 = function(x){log(x,2)} # log base 2
+nada = function(x){return(x)} # function that does nothing, used for unlisting lists
 
+# range of parameters in plot
 params = seq(0,60,5)
-p = as.character(params[2:13])
-eA_neg = c('0'=1, unlist(A['retro','negexp',p,'A']) / unlist(A['sched','negexp',p,'A']) )
-eA_cum = c('0'=1, unlist(A['retro','cum',p,'A']) / unlist(A['sched','cum',p,'A']) )
+ps = c(-30,-25,-20,-15,-10,-5,0,+5,+10)
 
 # empty plot
-par( mfrow=c(2,2), mar=c(2,4,.5,.4),family='serif') # bottom, left, top, right
-ps = c(-20,-15,-10,-5,0)
+#par( mfrow=c(2,2), mar=c(2,4,.5,.4),family='serif') # bottom, left, top, right
 plot( 0, type='n', 
-	xlim=c(range(params)), ylim=l2(c(.8,1.01)), 
+	xlim=c(0,60), ylim=l2(range(1+ps/100)), 
 	xaxt="n", yaxt="n", xlab='', ylab='', bty='n' 
 )
 # grid lines
@@ -24,7 +23,20 @@ axis( 2, at=l2(1+ps/100), labels=sprintf('%+.0f%%',ps), las=2 )
 # x axis
 axis( 1, at=c(0,30,60), labels=c('0','30 minutes','1 hour'), las=0 )
 # agency name
-text(x=0,y=l2(.81),labels=agency,pos=4,cex=2)
+text(x=0,y=l2(.77),labels=agency,pos=4,cex=2)
 # actual lines
-lines( x = params, y = l2(eA_cum), type='b' )
-lines( x = params, y = l2(eA_neg), type='b', col='red' )
+for(func in c('cum','negexp')){
+	color = switch(func,'cum'='red','negexp'='blue' )
+	eAh = sapply(A['retro',func,,'A_oh'],nada) / sapply(A['sched',func,,'A_oh'],nada)
+	# quartile range
+	ebars = cbind( c(1,1,1), apply(eAh,2,quantile,probs=c(.25,.5,.75),na.rm=T ) )
+	# plot the median
+	lines( x=params, y=l2(ebars[2,]), type='b',col=color )
+	# and the quartile range
+	polygon(
+		x=c(params,rev(params)), 
+		y=l2( c( ebars[1,],rev(ebars[3,]) ) ),
+		border=NA, col=adjustcolor(color,alpha=.2)
+	)
+}
+remove(eAh,ebars,ps,color)
